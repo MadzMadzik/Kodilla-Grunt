daeb1915cced943f47cd7705bda0d32496076680
MadzMadzik <mag.sobieszuk@gmail.com>
2017-05-02 14:13:19 +0200

    Added Imagein and gitignore

--- gruntfile.js
+++ gruntfile.js
@@ -35,14 +35,25 @@ module.exports = function(grunt) {
                     server: './'
                 }
             }
-        }
+        },
+    imagemin: {
+    dynamic: {
+        files: [{
+            expand: true,
+            cwd: 'images/',
+            src: ['**/*.{png,jpg,gif}'],
+            dest: 'images/build/'
+        }]
+    }
+},
   
   });
   // Load the plugins tasks 
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
   
   // Default task(s).
-  grunt.registerTask('default', ["sass", "browserSync", "watch"]);
+  grunt.registerTask('default', ["sass", "browserSync", "imagein" "watch"]);
 };
